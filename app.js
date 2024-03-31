e8097e0d77fdce31066be4353dff0c64eb342f9f
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2024-04-01 00:38:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+//  var cityToCheck = prompt("Enter your city");
+//  cityToCheck = cityToCheck.toLowerCase();
+//  var cleanestCities = ["karachi", "islamabd", "hyderabad", "multan", "lahore"];
+//  for (var i = 0; i <= 4; i++) {
+//  if (cityToCheck === cleanestCities[i]) {
+//  alert("It's one of the cleanest cities");
+//    }
+//  }
+
+
+// var str = prompt("Enter some text");
+//  for (var i = 0; i<str.length; i++) {
+//  if (str.slice(i, i + 2) === " ") {
+//  console.log("double spaces are not allowed");
+//  break;
+//  }
+
+
+// var text=prompt('Enter some text')
+// var flag=false
+// for(var i=0;i<text.length;i++){
+//     if (text.slice(i, i + 2)=== "  ") {
+//         document.write('double spaces are not allowed')
+//         flag=true
+//     break;
+//     }
+//     }
+// if (!flag) {
+//     document.write("Your password is corect  ")
+    
+// }
+
+
+/* Question1:
+var userinput=prompt('Enter your first name')
+var userinput1=prompt('Enter your last name')
+var fullname=userinput+userinput1
+document.write(fullname.concat())
+*/
+
+/*Question2:
+
+var MobileModal=prompt('Enter your favourite mobile modal ')
+document.write('My favourite Phone is:'+MobileModal+"<br>")
+var result
+result=MobileModal.length
+document.write('the length of string is\n'+result)
+*/
+
+/*
+var country='Pakistan'
+console.log(country)
+var result=country.indexOf('n')
+console.log('The index of n is:'+result)
+*/
+/*
+var country='Hello World'
+console.log(country)
+var result=country.lastIndexOf('l')
+console.log('The last index of n is:'+result)
+*/
+/*
+var city="Hyderabad"
+document.write(city+"<br>")
+ var result=city.replace('Hyder','Islama')
+document.write('The replacement is:'+ result)
+*/
+
+/*
+var message= "Ali and Sami are best friends. They play cricket and football together"
+document.write(message+"<br>")
+var replace=message.replaceAll("and","&")
+document.write(replace)
+*/
+
+/*
+var string="473"
+document.write(string+"<br>The type is string <br>")
+var num=Number(string)
+document.write(num+"<br>\n The type is number")
+
+*/
+
+
+
+/*
+var letter="peanunts"
+document.write(letter+"<br>")
+var uppercase= letter.toUpperCase()
+document.write(uppercase)
+*/
+
+
+/*
+var num=45.76
+document.write("Number is:\n"+num+"<br>")
+var string=num.toString()
+document.write("The result is:\n"+string.replace(".",""))
+*/
+
+/*
+var user=prompt("Enter your name")
+var characters=user.charCodeAt(0)
+if (characters>=33 & characters>=44 & characters>=46 & characters>=64) {
+    document.write('user name cannot contain special characters')
+}
+
+else{
+    document.write("correct username")
+}
+*/
+
+
+
+/*
+var university="University of Karachi"
+console.log(university)
+var convert=university.split()
+console.log(convert)
+*/
+
+/*
+var user=prompt("Enter a word")
+var LastCharacter=user.charAt(user.length -1)
+console.log(LastCharacter)
+*/
+
